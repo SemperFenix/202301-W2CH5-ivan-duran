f5bb503ce5e90769e4355d12d1d222836b50771f
SemperFenix <dureivan@gmail.com>
2023-01-29 10:49:57 +0100

    Add test for checkLastColumn

--- app.test.js
+++ app.test.js
@@ -104,6 +104,41 @@ describe("Given the checkPrevRowColumnZero function", () => {
   });
 });
 
+describe("Given the checkPrevRowFinalColumn function", () => {
+  describe("When the checked position is in column0", () => {
+    test("Then it should  return the number of active positions surrounding it using the last column as previous", () => {
+      const rowCheck = {
+        row0: {
+          column0: 0,
+          column1: 0,
+          column2: 1,
+          column3: 1,
+          column4: 1,
+          column5: 1,
+        },
+        row1: {
+          column0: 0,
+          column1: 0,
+          column2: 0,
+          column3: 0,
+          column4: 0,
+          column5: 0,
+        },
+        row2: {
+          column0: 1,
+          column1: 1,
+          column2: 1,
+          column3: 1,
+          column4: 1,
+          column5: 1,
+        },
+      };
+      const result = condition.checkPrevRowFinalColumn(rowCheck, 2, 5);
+      expect(result).toBe(3);
+    });
+  });
+});
+
 describe("Given the checkSameRowColumnZero function", () => {
   describe("When the checked position is in column0", () => {
     test("Then it should  return the number of active positions surrounding it using the last column as previous", () => {
